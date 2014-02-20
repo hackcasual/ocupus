a835e536f16f57552f396a0b98c0ec97db3d580b
Charles Vaughn <cvaughn@gmail.com>
2014-02-20 04:09:46 +0000

    Add support for v4l2-ctl support as well as running a processor script

--- scripts/ocupus_orchestrator.py
+++ scripts/ocupus_orchestrator.py
@@ -39,6 +39,8 @@ class Camera:
         self.device = None
         self.webrtc_device = None
         self.process_device = None
+        self.process_command = None
+        self.v4l2_ctl = None
     def __repr__(self):
         return str(self.name) + ": " + str(self.device)
 
@@ -59,8 +61,18 @@ for x in config.sections():
         name = x[7:]
         cam = Camera()
         cam.name = name
+
         cam.port = config.get(x, 'port')
-        cam.capabilities = config.get(x, 'capabilities')
+
+        options = config.options(x)
+
+        if 'capabilities' in options:
+            cam.capabilities = config.get(x, 'capabilities')
+        if 'v4l2settings' in options:
+            cam.v4l2_ctl = config.get(x, 'v4l2settings')
+        if 'processor' in options:
+            cam.process_command = config.get(x, 'processor')
+
         if cam.port in ports:
             cam.device = ports[cam.port]
             del ports[cam.port]
@@ -78,7 +90,7 @@ for sd in system_devices:
         cam.port = system_devices[sd].port_connection
         cam.device = ports[cam.port]
 
-        cam.name += str(cam.port)
+        cam.name += "::" + str(cam.port)
         cameras[cam.name] = cam
 
 try:
@@ -96,6 +108,15 @@ v4l2loopback_devices = {x for x in os.listdir("/dev/") if x.startswith("video")}
 
 v4l2loopback_devices.difference_update(current_devices)
 
+# Configure the camers
+for c in [z for z in cameras if cameras[z].v4l2_ctl]:
+    print("======================= Setting v4l2 controls for %s =======================" % cameras[c].name)
+
+    args = shlex.split("v4l2-ctl -d " + cameras[c].device + " " + cameras[c].v4l2_ctl)
+    proc = subprocess.call(args)
+
+
+# Set up the camers for splitting
 for c in cameras:
     cameras[c].webrtc_device = v4l2loopback_devices.pop()
     cameras[c].process_device = v4l2loopback_devices.pop()
@@ -108,14 +129,12 @@ for c in cameras:
         ' ! tee name=t ! queue2 ! v4l2sink sync=false device=/dev/' + cameras[c].webrtc_device +
         ' t. ! queue2 ! v4l2sink sync=false device=/dev/' + cameras[c].process_device)
 
-    print(["STWD::: "] + args)
     proc = subprocess.Popen(args)
     phandles.append(proc)
     # Hopefully this will give time to properly sync up the log statement above
-    time.sleep(1.100)
+    time.sleep(0.100)
 
-time.sleep(2.5)
-
+# Spawn the clients
 for c in cameras:
     print("======================= Connecting camera %s to peerconnection =======================" % cameras[c].name)
 
@@ -124,8 +143,20 @@ for c in cameras:
     	'" --videodevice /dev/' + cameras[c].webrtc_device)
     proc = subprocess.Popen(args)
     phandles.append(proc)
-    print(["STWD::: "] + args) 
-    time.sleep(2.100)
+    time.sleep(0.100)
+
+# Start subprocessors
+for c in [z for z in cameras if cameras[z].process_command]:
+    print("======================= Connecting camera %s to processor =======================" % cameras[c].name)
+
+    processor_env = os.environ.copy()
+    
+    processor_env["OCUPUS_CAMERA_DEV"] = cameras[c].process_device
+    args = shlex.split(cameras[c].process_command)
+    proc = subprocess.Popen(args, env=processor_env)
+    phandles.append(proc)
+    time.sleep(0.100)
+
 
 while True:
     time.sleep(5)
